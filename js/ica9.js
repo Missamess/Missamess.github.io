f0fbbe939fc90d661678a93827a0595ae733ecb2
Mayla Seliskar <jase2622@colorado.edu>
2023-03-15 23:38:12 -0600

    ica9

---
+++ js/ica9.js
@@ -0,0 +1,10 @@
+const name = document.querySelector(".name")
+
+function changeColor(color) {
+  // code 
+  name.style.color = color;
+}
+
+function resize(resize) {
+    name.style.height = height;
+}
